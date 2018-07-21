0b224cf01aaeb40fba60277843b1dfae1a9c37a8
Tyler Rhodes <tylerlrhodes@gmail.com>
2018-07-21 18:40:08 -0400

    2.29

--- CH2/ex2.28.scm
+++ CH2/ex2.28.scm
@@ -1,11 +1,12 @@
 #lang sicp
+
 (#%require "../Common/lib.scm")
 
 (define (fringe l)
   (define (iter l)
     (cond ((null? l) nil)
-          ((pair? (car l)) (cons (fringe (car l)) (fringe (cdr l))))
-          (else (cons (car l) (fringe (cdr l))))))
+          ((pair? (car l)) (append (fringe (car l)) (fringe (cdr l))))
+          (else (append (list (car l)) (fringe (cdr l))))))
   (iter l))
 
 (define x (list (list 1 2) (list 3 4)))
