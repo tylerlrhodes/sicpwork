23dbc4ec8e75a77d70f35791586c20ed42a0bdfb
Tyler Rhodes <tylerlrhodes@gmail.com>
2018-10-16 19:04:57 -0400

    more work...

---
+++ CH3/ch3-section2.scm
@@ -0,0 +1,21 @@
+#lang sicp
+
+;; 3.9
+
+;; not going to draw it here
+
+
+;; 3.10
+
+
+
+;; 3.11
+
+
+;; see:
+
+;; https://eli.thegreenplace.net/2007/09/28/sicp-section-32
+
+;; this site has some good drawings of the environments
+
+
