e22f1fc213a4319b4a9a1111cce20ab353ac47f7
Tyler Rhodes <tylerlrhodes@gmail.com>
2018-12-25 11:20:38 -0500

    3.24

--- CH3/ch3-section3.scm
+++ CH3/ch3-section3.scm
@@ -258,9 +258,53 @@
     dispatch))
 
 
+
+;; 3.24
+
+(define (make-table same-key?)
+  (let ((local-table (list '*table*)))
+    (define (assoc key records)
+      (cond ((null? records) #f)
+            ((same-key? key (caar records)) (car records))
+            (else (assoc key (cdr records)))))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  #f))
+            #f)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (list key-1
+                                        (cons key-2 value)
+                                        (cdr subtable))))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc) insert!)
+            (else (error "unknown operation -- TABLE" m))))
+    dispatch))
+
+(define op-table (make-table equal?))
+(define get (op-table 'lookup-proc))
+(define put (op-table 'insert-proc))
+
+
+
 ;; 3.26
 
-(define (make-table)
+(define (make-table-bst)
   (list '*table*))
 
 (define (lookup key table)
